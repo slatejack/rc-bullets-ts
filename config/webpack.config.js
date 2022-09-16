76a51e26c970aec6df5a94ca3714e34bcb8d23c3
slatejack <slatexu@gmail.com>
2022-09-16 11:53:27 +0800

    fix: 更改项目入口

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,7 +8,7 @@ const isDev = process.env.NODE_ENV === 'development';
 console.log('env:', process.env.NODE_ENV);
 
 module.exports = {
-  entry: './index.ts',
+  entry: './app.ts',
   devtool: isDev ? 'eval-cheap-source-map' : 'hidden-source-map',
   output: {
     path: resolve('../dist/'),
