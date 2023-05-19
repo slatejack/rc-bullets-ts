4ead93918ca5b6da244fa439adb86c9981749f3d
Jinyu <cadenho@hotmail.com>
2023-05-19 13:16:02 +0800

    refactor: 调整 app.ts 的位置，修正了 useBullentScreen 的拼写错误

--- app.ts
+++
@@ -1,6 +0,0 @@
-import BulletScreen from './src/bulletScreen';
-import StyledBullet from './src/styleBullet';
-import useBullentScreen from './src/hooks/useBulletScreen';
-
-export default BulletScreen;
-export { StyledBullet, useBullentScreen };
