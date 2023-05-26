b39b6d8c863fffed22e3d61b2ef9720044f87cfd
slatejack <1815846446@qq.com>
2023-05-26 18:28:51 +0800

    fix: 公共-变更app.ts位置

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-import BulletScreen from './bulletScreen';
-import StyledBullet from './styleBullet';
-import useBulletScreen from './hooks/useBulletScreen';
-
-export default BulletScreen;
-export { StyledBullet, useBulletScreen };
