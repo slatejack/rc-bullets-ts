fb2893749318138123f1dc64780ef114534a11d6
slatejack <1815846446@qq.com>
2023-05-26 18:44:03 +0800

    feat: 公共-新增typings, 发布v1.1.0

---
+++ typings/styleBullet.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { pushItemObj } from '@/interface/screen';
+import './style.scss';
+declare const StyledBullet: React.FC<pushItemObj>;
+export default StyledBullet;
